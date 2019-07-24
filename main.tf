1a56fed8c6adc652381d946527b60453b4ed0a70
Tom Stewart <=>
2019-07-24 06:47:44 -0400

    Argument names must not be quoted. - missing = for map

--- main.tf
+++ main.tf
@@ -28,11 +28,11 @@ resource "aws_api_gateway_method_response" "ok" {
   http_method = "${aws_api_gateway_method.api-method.http_method}"
   status_code = "200"
 
-  response_models {
+  response_models = {
     "application/json" = "Empty"
   }
 
-  response_parameters {
+  response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = true
     "method.response.header.Access-Control-Allow-Methods" = true
     "method.response.header.Access-Control-Allow-Origin" = true
@@ -50,7 +50,7 @@ resource "aws_api_gateway_integration_response" "ok-integration-response" {
     "application/json" = ""
   }
 
-  response_parameters {
+  response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
     "method.response.header.Access-Control-Allow-Methods" = "'${aws_api_gateway_method.api-method.http_method}'"
     "method.response.header.Access-Control-Allow-Origin" = "'*'"
